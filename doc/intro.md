dead49c025970acaaabf3859480535615ba51944
Michael Reilly <mikeyreilly@gmail.com>
2020-06-06 13:56:07 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mracme
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mreilly/mracme_test.clj
@@ -0,0 +1,7 @@
+(ns mreilly.mracme-test
+  (:require [clojure.test :refer :all]
+            [mreilly.mracme :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
